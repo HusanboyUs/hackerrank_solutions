e863105ab162b354acb713d3b588f912f4aeddb3
Husanboy Usmonov <husanghost@gmail.com>
2023-04-13 15:31:27 +0200

    Basic challange soulution for RunnerUpScore

---
+++ runnerUpscore.py
@@ -0,0 +1,11 @@
+tup=2,3,6,6,5
+
+my_array=set(tuple)
+my_list=list(my_array)
+my_list.sort()
+print(my_list[-2])
+
+'''
+Given the participants' score sheet for your University Sports Day, you are required 
+to find the runner-up score. You are given  scores. Store them in a list and find the score of the runner-up.
+'''
