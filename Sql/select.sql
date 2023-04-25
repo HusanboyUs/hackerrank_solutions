69e74d6cbf346008d16231593a99308af48d3358
Husanboy Usmonov <husanghost@gmail.com>
2023-04-25 20:32:56 +0200

    solved challanges

--- Sql/select.sql
+++ Sql/select.sql
@@ -10,3 +10,6 @@ SELECT NAME FROM CITY WHERE POPULATION>120000 AND COUNTRYCODE = "USA"
 --THIRD CHALLANGE
 --Query all columns (attributes) for every row in the CITY table.
 SELECT * FROM CITY;
+
+--Query all columns for a city in CITY with the ID 1661.
+SELECT * FROM CITY WHERE ID=1661;
