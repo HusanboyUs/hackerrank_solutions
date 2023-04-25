200dc6dc843dba950731765c2cadb99ac495f928
Husanboy Usmonov <husanghost@gmail.com>
2023-04-25 20:27:38 +0200

    solved challanges

--- Sql/select.sql
+++ Sql/select.sql
@@ -1,1 +1,10 @@
+--FIRST CHALLANGE
 SELECT * FROM CITY WHERE POPULATION>100000 AND COUNTRYCODE = "USA"
+
+--SECOND CHALLANGE
+--Query the NAME field for all American cities in the CITY table with populations larger than 120000. 
+--The CountryCode for America is USA.
+
+SELECT NAME FROM CITY WHERE POPULATION>120000 AND COUNTRYCODE = "USA"
+
+--THIRD CHALLANGE
