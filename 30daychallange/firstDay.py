7d839266c6132725dc3c02666fbadbc73c9f5321
Husanboy Usmonov <husanghost@gmail.com>
2023-04-25 20:02:55 +0200

    30 days of Python

---
+++ 30daychallange/firstDay.py
@@ -0,0 +1,6 @@
+inputString = input() # get a line of input from stdin and save it to our variable
+
+# Your first line of output goes here
+print('Hello, World.')
+print(inputString)
+# Write the second line of output
