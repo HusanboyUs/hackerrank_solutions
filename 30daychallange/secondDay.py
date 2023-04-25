18c908571bd6c7e83f89fd53f51b8b5feb531777
Husanboy Usmonov <husanghost@gmail.com>
2023-04-25 20:18:48 +0200

    Second day solution

---
+++ 30daychallange/secondDay.py
@@ -0,0 +1,16 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+num=int(input())
+flot=float(input())
+st=input()
+# Print the sum of both integer variables on a new line.
+print(i+num)
+# Print the sum of the double variables on a new line.
+print(d+flot)
+print("{}is the best place to learn and practice coding!".format(s))
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
