80b9c5700e299559a892fc0129242a05c0455d1f
Husanboy Usmonov <husanghost@gmail.com>
2023-05-03 22:16:27 +0200

    solved new challanges

--- Sql/select.sql
+++ Sql/select.sql
@@ -54,3 +54,7 @@ SELECT DISTINCT CITY FROM STATION WHERE RIGHT(CITY,1) NOT IN ('a','e','i','o','u
 SELECT DISTINCT CITY 
 FROM STATION 
 WHERE LEFT(CITY,1) NOT IN ('A','E','I','O','U') OR RIGHT(CITY,1) NOT IN ('A','E','I','O','U');
+
+
+--Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT CITY FROM STATION WHERE LEFT(CITY, 1) NOT IN ('A', 'E', 'I', 'O', 'U') AND RIGHT(CITY, 1) NOT IN ('A', 'E', 'I', 'O', 'U');
