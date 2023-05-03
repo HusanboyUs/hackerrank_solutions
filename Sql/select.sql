f319a5c7eee022fcd35a7471632a70a739c77679
Husanboy Usmonov <husanghost@gmail.com>
2023-05-03 22:19:03 +0200

    completed Weather Observation section challanges

--- Sql/select.sql
+++ Sql/select.sql
@@ -58,3 +58,13 @@ WHERE LEFT(CITY,1) NOT IN ('A','E','I','O','U') OR RIGHT(CITY,1) NOT IN ('A','E'
 
 --Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
 SELECT DISTINCT CITY FROM STATION WHERE LEFT(CITY, 1) NOT IN ('A', 'E', 'I', 'O', 'U') AND RIGHT(CITY, 1) NOT IN ('A', 'E', 'I', 'O', 'U');
+
+
+
+--Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters
+-- of each name. If two or more students both have names ending in the same last three characters 
+--(i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+SELECT Name
+FROM Students
+WHERE Marks > 75
+ORDER BY RIGHT(Name, 3), Id;
