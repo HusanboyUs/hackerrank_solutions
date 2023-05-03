7945b9eca74241a6e785951986aabef96e105dc9
Husanboy Usmonov <husanghost@gmail.com>
2023-05-03 21:11:35 +0200

    added new challange

--- Sql/select.sql
+++ Sql/select.sql
@@ -29,3 +29,8 @@ SELECT CITY,STATE FROM STATION
 SELECT DISTINCT CITY FROM STATION  WHERE mod(ID,2)=0;
 
 SELECT name FROM Employee ORDER BY name ASC;
+--Write a query that prints a list of employee names (i.e.: the name attribute) for employees 
+--in Employee having a salary greater than  per month who have been employees for less than  months. 
+--Sort your result by ascending employee_id.
+
+SELECT name FROM Employee WHERE salary > 2000 AND months <10 ORDER BY employee_id ASC;
