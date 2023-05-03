89e8785b05d4df93adb105890743597c3aa3500a
Husanboy Usmonov <husanghost@gmail.com>
2023-05-03 20:49:15 +0200

    solved ne challange

--- Sql/select.sql
+++ Sql/select.sql
@@ -22,3 +22,10 @@ SELECT NAME FROM CITY WHERE COUNTRYCODE ="JPN"
 
 --Query a list of CITY and STATE from the STATION table.
 SELECT CITY,STATE FROM STATION 
+
+--Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order,
+-- but exclude duplicates from the answer.
+--The STATION table is described as follows:
+SELECT DISTINCT CITY FROM STATION  WHERE mod(ID,2)=0;
+
+SELECT name FROM Employee ORDER BY name ASC;
