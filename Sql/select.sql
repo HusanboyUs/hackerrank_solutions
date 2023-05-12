7d3ce3c3e3ab3b7b9056c72bddf6f698037c7a62
Husanboy Usmonov <husanghost@gmail.com>
2023-05-12 15:18:38 +0200

    added new challange

--- Sql/select.sql
+++ Sql/select.sql
@@ -102,3 +102,6 @@ SELECT AVG(population) FROM CITY WHERE DISTRICT = "California"
 SELECT ROUND(AVG(POPULATION),0) FROM CITY 
 
 #new chapter
+
+--Query the sum of the populations for all Japanese cities in CITY. The COUNTRYCODE for Japan is JPN.
+SELECT sum(POPULATION) FROM CITY WHERE COUNTRYCODE = 'JPN'
