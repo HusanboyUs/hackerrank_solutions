70f12a7c5e0f6f2435321ae71ed8860616c25113
Husanboy Usmonov <husanghost@gmail.com>
2023-05-20 00:57:31 +0200

    solved challange

--- Sql/select.sql
+++ Sql/select.sql
@@ -117,3 +117,6 @@ SELECT city,LENGTH(city) FROM station ORDER BY LENGTH(city) DESC,city LIMIT 1;
 --Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION.
 -- Your result cannot contain duplicates.
 SELECT DISTINCT CITY FROM STATION WHERE LEFT(CITY,1) IN ('a','e','i','o','u')
+
+--Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+SELECT DISTINCT CITY FROM STATION WHERE RIGHT(CITY,1) in ('a','e','i','o','u')
