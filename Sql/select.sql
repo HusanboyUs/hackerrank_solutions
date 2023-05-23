0aba6c0ef1dd3062ea0365d1e53271cbe85c8409
Husanboy Usmonov <husanghost@gmail.com>
2023-05-23 19:37:29 +0200

    new challange solved

--- Sql/select.sql
+++ Sql/select.sql
@@ -136,3 +136,9 @@ From Employees
 
 --Weather observation station 2
 SELECT ROUND(SUM(LAT_N),2), ROUND(SUM(LONG_W),2) FROM STATION;
+
+
+--Weather observation station 13
+SELECT ROUND(SUM(LAT_N), 4)
+FROM STATION
+WHERE LAT_N > 38.7880 AND LAT_N < 137.2345
