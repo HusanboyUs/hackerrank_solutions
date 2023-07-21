90197fb3073610a61a42991100ebcd476b25e829
Husanboy Usmonov <husanghost@gmail.com>
2023-07-21 16:09:16 +0200

    added new solution

--- Leetcode/Python/66.py
+++ Leetcode/Python/66.py
@@ -1,6 +1,10 @@
+
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         digit = ''.join(map(str,digits))
         digit_add = int(digit) +1
         res= [int(x) for x in str(digit_add)]
         return res
+   
+
+
