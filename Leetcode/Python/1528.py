8ad163514fa94f56309e708145f88b3b262af567
Husanboy Usmonov <husanghost@gmail.com>
2023-08-04 21:30:03 +0200

    added new solution

---
+++ Leetcode/Python/1528.py
@@ -0,0 +1,21 @@
+s = "codeleet"
+indices = [4,5,6,7,0,2,1,3]
+
+
+#expected leetcode
+
+
+def shuffle(s,indices):
+    res = []
+      
+    return res                       
+
+
+
+
+print(shuffle(s,indices))    
+print(f'{indices}  -> how index should look like')
+result= []
+for indx,char in enumerate(list(s)):
+    result.append(indx)
+print(f'{result} -> current version')
