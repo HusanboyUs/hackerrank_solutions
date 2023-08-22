384def736022538df98e1a298c34b0f21659c6a0
Husanboy Usmonov <husanghost@gmail.com>
2023-08-22 14:47:48 +0200

    added a new solution

---
+++ Leetcode/Python/2062.py
@@ -0,0 +1,11 @@
+#not solved
+word ='aeiouu'
+
+def countVowelSubstrings(word):
+    vow = ['a','e','u','i','o']
+    res = []
+    for x in range(len(word)):
+        ...
+        
+
+print(countVowelSubstrings(word))    
