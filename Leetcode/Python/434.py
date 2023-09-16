80d8dad8b38d92eeb4f225e7597c11a5e0916be8
Husanboy Usmonov <husanghost@gmail.com>
2023-09-16 02:33:55 +0200

    added a new solution

---
+++ Leetcode/Python/434.py
@@ -0,0 +1,16 @@
+
+
+s = "Hello, my name is John"
+
+def countSegments(s: str) -> int:
+        return len(s.split())
+
+
+
+
+
+db = ['hello','alan walker','warsaw is capital of poland']
+
+for x in range(len(db)):
+    print(countSegments(s=db[x]))
+        
