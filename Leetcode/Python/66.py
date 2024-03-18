69316647516fee7d3a609582b300c0156590f806
Husanboy Usmonov <husanghost@gmail.com>
2024-03-18 02:05:25 +0100

    added a new solution

--- Leetcode/Python/66.py
+++ Leetcode/Python/66.py
@@ -1,10 +1,22 @@
 
-class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-        digit = ''.join(map(str,digits))
-        digit_add = int(digit) +1
-        res= [int(x) for x in str(digit_add)]
-        return res
+# class Solution:
+#     def plusOne(self, digits: List[int]) -> List[int]:
+#         digit = ''.join(map(str,digits))
+#         digit_add = int(digit) +1
+#         res= [int(x) for x in str(digit_add)]
+#         return res
    
+
+# class Solution(object):
+#     def plusOne(self, digits):
+#         """
+#         :type digits: List[int]
+#         :rtype: List[int]
+#         """
+#         nums = ''.join([str(x) for x in digits])
+#         total = int(nums) +1
+#         return [ int(x) for x in str(total)]
+
+
 
 
